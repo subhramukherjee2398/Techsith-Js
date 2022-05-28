083e763f3017d3682e07293c869a8132d9d01ff6
subhraWingsure <subhra@wingsure.co>
2022-05-28 12:06:42 +0530

    first commit

---
+++ Qns1.js
@@ -0,0 +1,13 @@
+//Q:1
+console.log([]+[])
+console.log({}+{})
+console.log({}+[])
+
+
+// here + operator will try to typecast [] to a string and after typoe casting it will return empty string
+
+// ex: console.log(string([]))  -> type casting [] to a string
+
+// so,
+
+  console.log(String([]))   // empty string.
